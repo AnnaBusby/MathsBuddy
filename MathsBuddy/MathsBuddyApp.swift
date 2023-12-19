2ce4742dfeb6f359925ef68dc8fd8d1e339a8d25
Anna Busby <anna.busby.services@outlook.com>
2023-12-19 19:53:59 +0000

    Initial Commit

---
+++ MathsBuddy/MathsBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathsBuddyApp.swift
+//  MathsBuddy
+//
+//  Created by Anna Busby on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MathsBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
